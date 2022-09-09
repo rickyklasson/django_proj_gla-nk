b7c436c1dae3fbeb105d359811a40a0aa52c48a3
Ricky Klasson <mrklasson@gmail.com>
2022-09-09 21:14:41 +0200

    Added entries in database

--- restaurant/views.py
+++ restaurant/views.py
@@ -5,12 +5,12 @@ from .models import MenuEntry, Review
 def index(request):
     # A la carté
     förrätter = MenuEntry.objects.all().filter(category__name__iexact='förrätter')
-    huvudrätter = MenuEntry.objects.all().filter(category__name__iexact='varmrätter')
-    schnitzel = MenuEntry.objects.all().filter(category__name__iexact='schnitzel')
+    varmrätter = MenuEntry.objects.all().filter(category__name__iexact='varmrätter')
+    schnitzel = MenuEntry.objects.all().filter(category__name__icontains='schnitzel')
     desserter = MenuEntry.objects.all().filter(category__name__iexact='desserter')
 
     a_la_carte = {'förrätter': förrätter,
-                  'huvudrätter': huvudrätter,
+                  'varmrätter': varmrätter,
                   'schnitzelkavalkad': schnitzel,
                   'desserter': desserter}
 
@@ -18,16 +18,11 @@ def index(request):
     fördrinkar = MenuEntry.objects.all().filter(category__name__iexact='fördrinkar')
     röda_viner = MenuEntry.objects.all().filter(category__name__iexact='röda viner')
     vita_viner = MenuEntry.objects.all().filter(category__name__iexact='vita viner')
-    sprit = MenuEntry.objects.all().filter(category__name__iexact='sprit')
-    övrigt = MenuEntry.objects.all().filter(category__name='Övrigt')
     
-    print(f'{övrigt=}')
 
     dryck_flags = [0, 1, 0, 0]
     dryck = {'fördrinkar': fördrinkar,
-             'viner': {'vita viner': vita_viner, 'röda viner': röda_viner},
-             'sprit': sprit,
-             'övrigt': övrigt}
+             'viner': {'vita viner': vita_viner, 'röda viner': röda_viner}}
 
     # Guestbook
     guest_book = Review.objects.all()
